516504612dd067e3d18ef6faa1a84910cb3f290d
AkselBF <aksel.boutrouefaret@yahoo.com>
2024-02-16 10:53:15 +0100

    Attemting to update and delete songs

--- routers/songRouters.js
+++ routers/songRouters.js
@@ -3,7 +3,9 @@ import express from 'express';
 import { 
   getAllSongs,
   getSongById,
-  createSong
+  createSong,
+  updateSong,
+  deleteSong
 } from '../controllers/songController.js';
 
 const router = express.Router();
@@ -11,5 +13,7 @@ const router = express.Router();
 router.get('/', getAllSongs);
 router.get('/:id', getSongById);
 router.post('/', createSong);
+router.put('/:id', updateSong);
+router.delete('/:id', deleteSong);
 
 export default router;
